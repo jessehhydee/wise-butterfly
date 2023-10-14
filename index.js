729f9bf5206240dcd1db336783b6b93d1dd8c3ec
jessehhydee <jesse@custombased.com>
2023-10-15 10:29:26 +1300

    added fog

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ const setScene = async () => {
   };
 
   scene = new THREE.Scene();
+  scene.background  = new THREE.Color(0x0B1C25);
 
   camera = new THREE.PerspectiveCamera(60, sizes.width / sizes.height, 1, 400);
   camera.position.set(0, 60, 80);
@@ -50,7 +51,6 @@ const setScene = async () => {
   renderer = new THREE.WebGLRenderer({
     canvas:     canvas,
     antialias:  false,
-    alpha:      true
   });
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
   clock = new THREE.Clock()
@@ -67,6 +67,7 @@ const setScene = async () => {
   currentLookAt           = new THREE.Vector3();
   currentLookAtLerpObj    = new THREE.Object3D();
 
+  setFog();
   await createChar();
   setTileValues();
   createTile();
@@ -79,6 +80,43 @@ const setScene = async () => {
   sceneRendered = true;
 
 };
+
+const setFog = () => {
+
+  THREE.ShaderChunk.fog_pars_vertex += `
+    #ifdef USE_FOG
+      varying vec3 vWorldPosition;
+    #endif
+  `;
+
+  THREE.ShaderChunk.fog_vertex += `
+    #ifdef USE_FOG
+      vec4 worldPosition = projectionMatrix * modelMatrix * vec4(position, 1.0);
+      vWorldPosition = worldPosition.xyz;
+    #endif
+  `;
+
+  THREE.ShaderChunk.fog_pars_fragment += `
+    #ifdef USE_FOG
+      varying vec3 vWorldPosition;
+      float fogHeight = 10.0;
+    #endif
+  `;
+
+  const FOG_APPLIED_LINE = 'gl_FragColor.rgb = mix( gl_FragColor.rgb, fogColor, fogFactor );';
+  THREE.ShaderChunk.fog_fragment = THREE.ShaderChunk.fog_fragment.replace(FOG_APPLIED_LINE, `
+    float heightStep = smoothstep(fogHeight, 0.0, vWorldPosition.y);
+    float fogFactorHeight = smoothstep( fogNear * 0.7, fogFar, vFogDepth );
+    float fogFactorMergeHeight = fogFactorHeight * heightStep;
+    
+    gl_FragColor.rgb = mix( gl_FragColor.rgb, fogColor, fogFactorMergeHeight );
+    ${FOG_APPLIED_LINE}
+  `);
+
+  scene.fog = new THREE.Fog(0x0B1C25, 200, 240);
+
+}
+
 
 const createChar = async () => {
 
